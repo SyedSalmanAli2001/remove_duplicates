fcaeba6d4cde658ae8f01c63b4b8396bec8080a7
SyedSalmanAli2001 <syed.salman.ali@outlook.com>
2024-08-12 18:12:26 +0500

    working solution

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 nums = [2, 4, 12, 50, 50, 50, 90, 90]
 
-for i in range(len(nums)-1):
-    if nums[i+1] == nums[i]:
-        nums.remove(nums[i])
+i = 0
+while i < len(nums) - 1:
+    if nums[i] == nums[i+1]:
+        nums.pop(i)
+    else:
+        i += 1
 
 print(nums)
