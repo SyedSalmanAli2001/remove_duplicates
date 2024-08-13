afc2258c18a3e8f6fc71968d0cf2c2b4719d15d8
SyedSalmanAli2001 <syed.salman.ali@outlook.com>
2024-08-13 14:27:04 +0500

    making

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 nums = [2, 4, 12, 50, 50, 50, 90, 90]
+#
+# i = 0
+# while i < len(nums) - 1:
+#     if nums[i] == nums[i+1]:
+#         nums.pop(i)
+#     else:
+#         i += 1
+#
+# print(nums)
 
-i = 0
-while i < len(nums) - 1:
-    if nums[i] == nums[i+1]:
-        nums.pop(i)
-    else:
-        i += 1
 
-print(nums)
+def remove_duplicates(nums_array):
+    i = 0
+    while i < len(nums_array) - 1:
+        if nums_array[i] == nums_array[i + 1]:
+            nums_array.pop(i)
+        else:
+            i += 1
+    return len(nums_array)
+
+
+print(remove_duplicates(nums))
