3a41452e19de58484e915e8c84f64693c932103e
SyedSalmanAli2001 <syed.salman.ali@outlook.com>
2024-08-15 15:21:30 +0500

    more efficient solution

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-nums = [2, 4, 12, 50, 50, 50, 90, 90]
+
 #
 # i = 0
 # while i < len(nums) - 1:
@@ -10,14 +10,32 @@ nums = [2, 4, 12, 50, 50, 50, 90, 90]
 # print(nums)
 
 
-def remove_duplicates(nums_array):
-    i = 0
-    while i < len(nums_array) - 1:
-        if nums_array[i] == nums_array[i + 1]:
-            nums_array.pop(i)
-        else:
-            i += 1
-    return len(nums_array)
+# def remove_duplicates(nums_array):
+#     i = 0
+#     while i < len(nums_array) - 1:
+#         if nums_array[i] == nums_array[i + 1]:
+#             nums_array.pop(i)
+#         else:
+#             i += 1
+#     return len(nums_array)
+#
+#
+# print(remove_duplicates(nums))
+
+nums = [2, 4, 12, 50, 50, 50, 90, 90, 91, 91, 2]
+# given array is already sorted
 
 
-print(remove_duplicates(nums))
+def remove_duplicates(nums_arr):
+    j = 0
+    for i in range(1, len(nums_arr)):
+        if nums_arr[i] != nums_arr[j]:
+            j += 1
+            nums_arr[j] = nums_arr[i]
+    return j + 1
+
+
+unique_count = remove_duplicates(nums)
+print(unique_count)
+print(nums[:unique_count])
+
